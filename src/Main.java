440b3e74f19d6480eea9cf386eb62ccbebca5e09
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-05 10:07:42 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d", age);
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+
+        System.out.printf("Your age is %.2f%n", (float) age);
+
+        for (int i = 1; i <= 100000; i *= 10){
+            System.out.printf("Printing %6d %n", i);
+        }
+
+        String formattedString = String.format("Your age is %d", age );
+        System.out.println(formattedString);
+
+        formattedString = "your age is %d".formatted(age);
+        System.out.println(formattedString);
+
     }
 }
